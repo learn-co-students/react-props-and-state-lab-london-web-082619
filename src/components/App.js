9410fade1ca0b857f6c2c25a6abb189256f58612
Mike Thurgood <mikecthurgood@gmail.com>
2019-10-29 13:43:07 +0000

    Done.

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,56 @@
 import React from 'react'
-
 import Filters from './Filters'
 import PetBrowser from './PetBrowser'
 
 class App extends React.Component {
+
+  onChangeType = (e) => {
+    this.setState({
+      ...this.state,
+      filters: {
+        type: e.target.value
+      }
+    })
+  }
+
+  onAdoptPet = (id) => {
+    this.setState({
+      pets: this.state.pets.map(pet => {
+        // debugger
+        if (pet.id !== id) return pet
+        return {
+          ...pet,
+          isAdopted: true
+        }
+      })
+    })
+  }
+
+  fetchAPI = URL => {
+    fetch(URL)
+      .then(resp => resp.json())
+      .then(json => {
+        this.setState({ ...this.state, pets: json })
+        // debugger
+      })
+  }
+
+  onFindPetsClick = () => {
+    console.log('arrrgggghhhh')
+    const type = this.state.filters.type
+    const API = '/api/pets'
+    switch (type) {
+      case 'all': this.fetchAPI(API)
+        break;
+      case 'cat': this.fetchAPI(`${API}?type=cat`)
+        break;
+      case 'dog': this.fetchAPI(`${API}?type=dog`)
+        break;
+      case 'micropig': this.fetchAPI(`${API}?type=micropig`)
+        break;
+    }
+  }
+
   constructor() {
     super()
 
@@ -15,6 +62,7 @@ class App extends React.Component {
     }
   }
 
+
   render() {
     return (
       <div className="ui container">
@@ -24,10 +72,10 @@ class App extends React.Component {
         <div className="ui container">
           <div className="ui grid">
             <div className="four wide column">
-              <Filters />
+              <Filters onChangeType={this.onChangeType} onFindPetsClick={this.onFindPetsClick} />
             </div>
             <div className="twelve wide column">
-              <PetBrowser />
+              <PetBrowser pets={this.state.pets} onAdoptPet={this.onAdoptPet} />
             </div>
           </div>
         </div>
