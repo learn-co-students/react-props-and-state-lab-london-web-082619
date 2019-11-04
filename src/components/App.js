1b91414d1374728cc19f77cbef577be3ef475bc1
ck2e14 <chriswkennedy@icloud.com>
2019-11-04 21:21:20 +0000

    Done.

--- src/components/App.js
+++ src/components/App.js
@@ -36,7 +36,7 @@ class App extends React.Component {
     .then(pets => this.setState({ pets }))
 
   }
-  
+  // the below method has a ternery operator. If the passed-in petID matches the pet id that is currently being iterated over (from App's state's pet array) then it will return a new object which firstly spreads the existing key:value pairs from the pet into a new object and then sets the isAdopted value to true. If it's not found then it just returns unchanged array, 'pets', which is then passed as an object into the setState method. So basically when u click the button to adopt it iterates over every single pet, and when it finds a pet that matches the passed in id (which is taken from the event itself, see Pet.js) it will update the state key of isAdopted to true. Fuckin yeet
   onAdoptPet = petId => {
     const pets = this.state.pets.map(p => {
       return p.id === petId ? {...p, isAdopted: true } : p;
